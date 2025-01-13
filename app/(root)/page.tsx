4fab2ec3097dea710b5b41e85f5e57c8259803a9
Upamanyu Ghosh <upamanyu179@gmail.com>
2025-01-13 17:19:31 +0530

    added basic ui of the home page and input fields

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,31 @@
+import SearchForm from "@/components/SearchForm";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home({ searchParams } : {
+  searchParams: Promise<{query? : string}>
+}) {
+
+  const query = (await searchParams).query
+
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your Startup, <br /> Connect with Entrepreneur
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Putches, and Get Noticed in Virtural Competitions.
+        </p>
+
+        <SearchForm query={query}/>
+
+
+      </section>
+
+
+      
     </>
   );
 }
+ 
