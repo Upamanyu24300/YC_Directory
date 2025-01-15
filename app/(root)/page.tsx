efdaec602753403477d16cb5a8a8f498bdfa68aa
Upamanyu Ghosh <upamanyu179@gmail.com>
2025-01-15 21:52:13 +0530

    modifying sanity and creating schemas

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import Image from "next/image";
 
 export default async function Home({ searchParams } : {
@@ -7,21 +10,43 @@ export default async function Home({ searchParams } : {
 
   const query = (await searchParams).query
 
+  // const posts = await client.fetch(STARTUPS_QUERY)
+  const { data: posts} = await sanityFetch({query: STARTUPS_QUERY})
+
+
+
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch your Startup, <br /> Connect with Entrepreneur
+          Pitch your Startup, <br /> Connect with Entrepreneurs
         </h1>
 
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Putches, and Get Noticed in Virtural Competitions.
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtural Competitions.
         </p>
-
+ 
         <SearchForm query={query}/>
 
-
       </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ):(
+              <p className="no-results"> No Startups Found</p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
 
 
       
